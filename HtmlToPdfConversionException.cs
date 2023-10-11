b73820418e8ffe1eda8a8e0dc644a2cbf47a1426
Shaziahayat <shazia.hayat@hotmail.com>
2023-10-11 09:32:45 +0200

    Add project files.

---
+++ HtmlToPdfConversionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ConsoleApp4
+{
+    [Serializable]
+    internal class HtmlToPdfConversionException : Exception
+    {
+        public HtmlToPdfConversionException()
+        {
+        }
+
+        public HtmlToPdfConversionException(string message) : base(message)
+        {
+        }
+
+        public HtmlToPdfConversionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected HtmlToPdfConversionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Syncfusion.HtmlConverter;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Syncfusion.Pdf;
+
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.Blink);
+            //Initialize HTML to PDF converter with Blink rendering engine
+            HtmlToPdfConverter htmlConverter = new HtmlToPdfConverter(HtmlRenderingEngine.Blink);
+            BlinkConverterSettings blinkConverterSettings = new BlinkConverterSettings();
+            //Set the BlinkBinaries folder path
+            blinkConverterSettings.BlinkPath = @"C:\Users\vismun\source\repos\ConsoleApp4\packages\Syncfusion.HtmlToPdfConverter.Net.Windows.23.1.40\runtimes";
+            //Assign Blink converter settings to HTML converter
+            htmlConverter.ConverterSettings = blinkConverterSettings;
+            //Convert URL to PDF
+            PdfDocument document = htmlConverter.Convert("https://www.google.com");
+            //Save and close the PDF document 
+
+            FileStream fileStream = new FileStream("HTML-to-PDF.pdf", FileMode.CreateNew, FileAccess.ReadWrite);
+
+            document.Save(fileStream);
+            document.Close(true);
+
+        }
+
+    }
+    
+}
